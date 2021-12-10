ac8bb5db56f2d3e611cf552a1c7b1e2d4a346bf4
jk4g2 <jk4g2@mail.missoui.edu>
2021-12-10 16:37:13 +0900

    Added VO classes

--- Dogether/src/main/java/com/dogether/domain/BoardVO.java
+++ Dogether/src/main/java/com/dogether/domain/BoardVO.java
@@ -1,62 +1,64 @@
 package com.dogether.domain;
 
 public class BoardVO {
-	private int BoardID;
-	private String  MemberID;
-	private String  BoardTitle;
-	private String  BoardContent;
-	private String  Board_fname;
-	private String  Board_realfname;
-	private int fsize;
-	private int Board_like_count;
+	private int boardID;
+	private String memberID;
+	private String boardTitle;
+	private String boardContent;
+	private String board_fname;
+	private String board_realfname;
+	private int board_fsize;
+	private int board_like_count;
 	
 	public int getBoardID() {
-		return BoardID;
+		return boardID;
 	}
 	public void setBoardID(int boardID) {
-		BoardID = boardID;
+		this.boardID = boardID;
 	}
 	public String getMemberID() {
-		return MemberID;
+		return memberID;
 	}
 	public void setMemberID(String memberID) {
-		MemberID = memberID;
+		this.memberID = memberID;
 	}
 	public String getBoardTitle() {
-		return BoardTitle;
+		return boardTitle;
 	}
 	public void setBoardTitle(String boardTitle) {
-		BoardTitle = boardTitle;
+		this.boardTitle = boardTitle;
 	}
 	public String getBoardContent() {
-		return BoardContent;
+		return boardContent;
 	}
 	public void setBoardContent(String boardContent) {
-		BoardContent = boardContent;
+		this.boardContent = boardContent;
 	}
 	public String getBoard_fname() {
-		return Board_fname;
+		return board_fname;
 	}
 	public void setBoard_fname(String board_fname) {
-		Board_fname = board_fname;
+		this.board_fname = board_fname;
 	}
 	public String getBoard_realfname() {
-		return Board_realfname;
+		return board_realfname;
 	}
 	public void setBoard_realfname(String board_realfname) {
-		Board_realfname = board_realfname;
+		this.board_realfname = board_realfname;
 	}
-	public int getFsize() {
-		return fsize;
+	public int getBoard_fsize() {
+		return board_fsize;
 	}
-	public void setFsize(int fsize) {
-		this.fsize = fsize;
+	public void setBoard_fsize(int board_fsize) {
+		this.board_fsize = board_fsize;
 	}
 	public int getBoard_like_count() {
-		return Board_like_count;
+		return board_like_count;
 	}
 	public void setBoard_like_count(int board_like_count) {
-		Board_like_count = board_like_count;
+		this.board_like_count = board_like_count;
 	}
+	
+
 	
 }
