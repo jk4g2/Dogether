0499c126d50fd98358a5bd06eb8394d93d63be44
Dawon Kim <dawn4326@naver.com>
2021-12-22 16:19:37 +0900

    테스트

--- Dogether/src/main/java/com/dogether/domain/Board_ReplyVO.java
+++ Dogether/src/main/java/com/dogether/domain/Board_ReplyVO.java
@@ -6,4 +6,35 @@ public class Board_ReplyVO {
 	private String reply;
 	private String replyer;
 	private String ReplyDate;
+	public int getReply_no() {
+		return reply_no;
+	}
+	public void setReply_no(int reply_no) {
+		this.reply_no = reply_no;
+	}
+	public int getBoardID() {
+		return boardID;
+	}
+	public void setBoardID(int boardID) {
+		this.boardID = boardID;
+	}
+	public String getReply() {
+		return reply;
+	}
+	public void setReply(String reply) {
+		this.reply = reply;
+	}
+	public String getReplyer() {
+		return replyer;
+	}
+	public void setReplyer(String replyer) {
+		this.replyer = replyer;
+	}
+	public String getReplyDate() {
+		return ReplyDate;
+	}
+	public void setReplyDate(String replyDate) {
+		ReplyDate = replyDate;
+	}
+	
 }
