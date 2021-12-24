d74e4167c4fa823454528734e915284f53dd8fb6
jk4g2 <jk4g2@mail.missoui.edu>
2021-12-24 11:41:43 +0900

    MemberDAO

--- Dogether/src/main/java/com/dogether/dao/MemberDAO.java
+++ Dogether/src/main/java/com/dogether/dao/MemberDAO.java
@@ -1,9 +1,15 @@
 package com.dogether.dao;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import com.dogether.domain.BoardVO;
 import com.dogether.domain.MemberVO;
 
 public interface MemberDAO {
 	public List<MemberVO> getMemberList(MemberVO vo);
+	public MemberVO getMemberInfo(String memberID);
+	public void updatePoint(String memberID,int price);
+	public List<BoardVO> myBoardList(String memberID);
 }
